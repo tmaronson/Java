0a06b3d7d37bec05f9be3084f812c10ee5cf8005
Tom Aronson <tmaronson@gmail.com>
2023-05-18 22:11:57 -0400

    Add files via upload

---
+++ books/src/main/java/com/oreilly/App.java
@@ -0,0 +1,13 @@
+package com.oreilly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
